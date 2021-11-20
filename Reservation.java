512f5026aa64e5b93054572feac7e164f1f47ec2
dom <74149150+kiing-dom@users.noreply.github.com>
2021-11-20 19:11:39 +0000

    Update Reservation.java
    Added comments to the methods

--- Reservation.java
+++ Reservation.java
@@ -13,35 +13,35 @@ public class Reservation {
         this.to = to;
 
     }
-
+    /** get the current Description */
     public String getDescription() {
         return this.description;
     }
-
+    /** set a new description */
     public void setDescription(String description) {
         this.description = description;
     }
-
+    /** get the current day */
     public ReservationDate getDay() {
         return this.day;
     }
-
+    /** set a new day */
     public void setDay(ReservationDate day) {
         this.day = day;
     }
-
+    /** get the start time */
     public ReservationTime getFrom() {
         return this.from;
     }
-
+    /** Set a new start time */
     public void setFrom(ReservationTime from) {
         this.from = from;
     }
-
+    /** Get the end time */
     public ReservationTime getTo() {
         return this.to;
     }
-
+    /** Set a new end time */
     public void setTo(ReservationTime to) {
         this.to = to;
     }
