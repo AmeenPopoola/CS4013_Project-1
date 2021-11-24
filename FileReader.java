8aefd2b416a01f84dae561534d255e5bd5f05f30
AmeenPopoola <82396792+AmeenPopoola@users.noreply.github.com>
2021-11-24 19:05:55 +0000

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class FileReader {
     private String [][] info;
+    private String [][] Reservations;
 
 public String [][] readHotels() throws FileNotFoundException{
     java.io.File file = new java.io.File(".../l4Hotels.csv");
@@ -20,6 +21,36 @@ public String [][] readHotels() throws FileNotFoundException{
     }
     input.close();
     return info;
+}
+
+public String [][] readRes() throws FileNotFoundException{
+    java.io.File file = new java.io.File(".../Reservations.csv");
+    Scanner count = new Scanner(file);
+    Scanner input = new Scanner(file);
+
+    String line ;
+    String c;
+    int z = 0;
+
+    // Row Count
+    while(count.hasNext()){
+       c = count.nextLine();
+       z++;
+    }
+    count.close();
+
+    this.Reservations =new String [z][9];
+
+    while(input.hasNext()){
+        line = input.nextLine();
+        this.Reservations[z] = line.split(",");
+        z++;
+    }
+    input.close();
+    return Reservations;
+
+
+
 }
 
 }
