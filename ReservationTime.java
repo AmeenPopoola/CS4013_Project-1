8604ba2aaeabfb64f0fbc64a892a1c4e85495d2b
Dominion Gbadamosi <74149150+kiing-dom@users.noreply.github.com>
2021-11-25 21:07:59 +0000

    Add files via upload

---
+++ ReservationTime.java
@@ -0,0 +1,27 @@
+public class ReservationTime {
+    private String from;
+    private String to;
+
+    public ReservationTime(String from, String to){
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    
+}
