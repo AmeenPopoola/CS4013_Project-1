795ae2eb2bfb397ef48b6fae9c8d5a3fdd6db9e7
michealmul <94451033+michealmul@users.noreply.github.com>
2021-11-26 15:07:38 +0000

    Update Hotels.java

--- Hotels.java
+++ Hotels.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
 public class Hotels {
-    private int rooms_available;
+    private String rooms_available;
     private int cost;
     private int length_of_stay;
-    private int Hotel_rating;
+    
     
     public Hotels() {
     }
-    public Hotels(int rooms_available,int cost, int length_of_stay,int Hotel_rating) {
+    public Hotels(String rooms_available,int cost, int length_of_stay,int Hotel_rating) {
     }
     public void setCost(int cost) {
         this.cost = cost;
@@ -15,35 +14,20 @@ public class Hotels {
         public void setLength_of_stay(int length_of_stay) {
         this.length_of_stay = length_of_stay;
     }
-        public void setRooms_available(int rooms_available) {
+        public void setRooms_available(String rooms_available) {
         this.rooms_available = rooms_available;
     }
-            public void setHotel_rating(int Hotel_rating) {
-        this.Hotel_rating = Hotel_rating;
-    }
+
     
     public int getCost() {
         return(cost);
     }
-        public int getRooms_available() {
+        public String getRooms_available() {
         return(rooms_available);
     }
-        public int getHotel_rating() {
-        return(Hotel_rating);
-    }
+
         public int getlength_of_stay() {
         return(length_of_stay);
     }
-    //L4 owns a chain of hotels, at present they own a Five star a four star and a three star 
-    ArrayList<String> Hotels = new ArrayList<String>();
-    public void addStarHotels() {
-    Hotels.add("Five Star");
-    Hotels.add("Four Star");
-    Hotels.add("Three Star");
-}
-    
-    public void addHotels(String Hotel) {
-        //according to the specificationL4 could possibly purchase more hotels so this feature must be added
-        Hotels.add(Hotel);
-    }
+
 }
