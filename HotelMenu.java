067431669402744956c88ca5d63e686f1a4c7e63
rian-ol <64734797+rian-ol@users.noreply.github.com>
2021-11-26 18:38:25 +0000

    Add files via upload

---
+++ HotelMenu.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+/**
+ A menu for the appointment calendar system.
+ */
+public class HotelMenu
+{
+    private Scanner in;
+
+    /**
+     Constructs an AppointmentMenu object.
+     */
+    public HotelMenu()
+    {
+        in = new Scanner(System.in);
+    }
+
+    /**
+     Runs the system.
+     */
+    public void run() {
+        boolean more = true;
+        ReservationCalendar calendar = new ReservationCalendar();
+
+        while (more) {
+
+        }
+    }
+}
