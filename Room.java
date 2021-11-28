24b1bac986130253d08d5e6121018f85f5464824
AmeenPopoola <82396792+AmeenPopoola@users.noreply.github.com>
2021-11-28 21:16:57 +0000

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,22 +1,28 @@
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.Arrays;
+
 public class Room {
 
     private String roomType;
     private int minOccupancy;
     private int maxOccupancy;
     private double[] rates;
-    private ArrayList<Reservation> booked = new ArrayList<Reservation>();
+    private ArrayList<Reservation> booked;
 
-    public Room(String roomType , int minOccupancy , int maxOccupancy , double[] rates){
+    public Room(String roomType , int minOccupancy , int maxOccupancy , double[] rates , ArrayList<Reservation> booked ){
         this.roomType = roomType;
         this.minOccupancy= minOccupancy;
         this.maxOccupancy= maxOccupancy;
         this.rates = rates;
+        this.booked = new ArrayList<>();
     }
+
     public ArrayList<Reservation> getBooked(){
         return booked;
     }
-    
-      public void setMaxOccupancy(int maxOccupancy) {
+
+    public void setMaxOccupancy(int maxOccupancy) {
         this.maxOccupancy = maxOccupancy;
     }
 
@@ -47,4 +53,41 @@ public class Room {
     public double[] getRates() {
         return rates;
     }
-}
+
+    /**
+     * Checks if room is available
+     */
+
+    public boolean checkAvailablity(Reservation res , Hotels hot , GregorianCalendar dateFrom , GregorianCalendar dateTo){
+        GregorianCalendar from = null ;
+        GregorianCalendar to = null;
+        int pos = 0;
+
+        for(int i = 0; i < hot.getArray(hot.getChoice()).size(); i++){
+            if(roomType.equals(hot.getArray(hot.getChoice()).get(i).getRoomType())){
+                if (hot.getArray(hot.getChoice()).get(i).getBooked().size() > 0) {
+                    for (int j = 0; j < hot.getArray(hot.getChoice()).get(i).getBooked.size(); j++) {
+                        from = hot.getArray(hot.getChoice()).get(i).getBooked(j).getDateFrom();
+                        to = hot.getArray(hot.getChoice()).get(i).getBooked(j).getDateTo();
+
+                        if (dateFrom.before(from) && dateTo.before(to) || dateFrom.after(from) && dateTo.after(to)) {
+                            pos = i;
+                            hot.getArray(hot.getChoice()).get(pos).getBooked().add(res);
+                            return true;
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    hot.getArray(hot.getChoice()).get(i).getBooked().add(res);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+                            
+                    
+                    }
+                    
+                
