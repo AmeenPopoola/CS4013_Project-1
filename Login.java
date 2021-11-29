34478ac6ea5b2c9bddb39874b5058ab9169f8322
rian-ol <64734797+rian-ol@users.noreply.github.com>
2021-11-29 13:58:35 +0000

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,22 +1,32 @@
 import java.io.*;
-import java.io.FileReader;
 import java.util.*;
 
 public class Login {
-    public static void main(String[] args) {
-        getAccess("ciarabrod", "ciarabrod");
-    }
     private String username;
     private String password;
+    private Boolean isAdmin = false;
+
     public Login(String username, String password){
         this.username = username;
         this.password = password;
     }
-    public static boolean getAccess(String user, String pass){
+
+    // Return the role of the user if logged in.
+    public Boolean getIsAdmin () {
+        return isAdmin;
+    }
+
+    // Return the username of the user that logged in.
+    public String getUsername () {
+        return username;
+    }
+
+    public boolean getAccess(/*String user, String pass*/){
         try {
             /**Reading the file and adding the password and username to an array
              * along with the access that the person is given (admin or customer)
-            **/
+             **/
+            
             java.io.File File = new java.io.File("src/main/resources/UsernameAndPasswords.csv");
             Scanner input = new Scanner(File);
             Scanner count = new Scanner(File);
@@ -39,50 +49,37 @@ public class Login {
                 line = input.nextLine();
                 userAndPass[i] = line.split(",");
                 i++;
-                //System.out.println(line + i);
             }
             input.close();
+
             /**
              * Checks if both the username and password are equal, and if they are
              * checks what kind of role the user is allowed to have in the system.
              */
+
+
             boolean isAuth = false;
-            boolean adminOrNot = false;
-           for (int j =1;j< userAndPass.length-1;j++) {
-                String checkUserEqual = user.toLowerCase(Locale.ROOT);
+            for (int j =1;j< userAndPass.length-1;j++) {
+                String checkUserEqual = this.username.toLowerCase(Locale.ROOT);
                 String authCheckUser = userAndPass[j][0].toString().toLowerCase(Locale.ROOT);
-                String authCheckPass = userAndPass[j][1].toString();
+                String authCheckPassword = userAndPass[j][1].toString();
                 String authCheckRole = userAndPass[j][2].toString().toLowerCase(Locale.ROOT);
-                if (authCheckUser.equals(checkUserEqual)){
-                   if (authCheckPass.equals(pass)){
-                       isAuth = true;
-                       if (authCheckRole.equals("admin")){
-                           adminOrNot = true;
-                           break;
-                       }
-                   }
+                if ((authCheckUser.equals(checkUserEqual)) && (authCheckPassword.equals(this.password))){
+                    isAuth = true;
+                    this.isAdmin = authCheckRole.equals("admin");
+                    break;
                 }
-
-
             }
-            if (!isAuth){
+
+            if (!isAuth) {
                 System.out.println("You have inputted an incorrect username or password");
-
             }
-            return adminOrNot;
-        }
 
+            return isAuth;
+        }
         catch (FileNotFoundException e){
             System.out.println("This file is missing or the file path is incorrect");
         }
         return false;
     }
-
-
-
-
-
-
-
 }
-
