8432ded54d942f7ccf5d3c1cb86b4b67fe4d17dd
rian-ol <64734797+rian-ol@users.noreply.github.com>
2021-11-30 11:04:48 +0000

    Update HotelSystem.java

--- HotelSystem.java
+++ HotelSystem.java
@@ -1,10 +1,11 @@
 import java.io.IOException;
 
 /**
- A system to manage appointments.
+ A system to manage reservations.
+ Creates a new menu object that is then ran.
  */
 public class HotelSystem
-{
+{   
     public static void main(String[] args)
             throws IOException
     {
