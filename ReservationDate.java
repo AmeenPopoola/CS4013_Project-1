7e16e3130c2564dd23f8321ac2618dfa02528826
rian-ol <64734797+rian-ol@users.noreply.github.com>
2021-11-30 06:57:49 +0000

    Add files via upload

---
+++ ReservationDate.java
@@ -0,0 +1,41 @@
+public class ReservationDate{
+
+    private int year;
+    private int month;
+    private int day;
+
+    public ReservationDate(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    public int getDay(){
+        return this.day;
+    }
+
+    public void setDay(int day){
+        this.day = day;
+    }
+
+    public int getMonth(){
+        return this.month;
+    }
+
+    public void setMonth(int month){
+        this.month = month;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+
+
+
+    }
+    public String format(){
+        return this.day + "/" + this.month + "/" + this.year;
+    }
+}
