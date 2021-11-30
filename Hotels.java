e03b63638b21a1c3fa35c715778cede5bec45f23
rian-ol <64734797+rian-ol@users.noreply.github.com>
2021-11-30 11:16:02 +0000

    Update Hotels.java

--- Hotels.java
+++ Hotels.java
@@ -1,68 +1,99 @@
 import java.util.ArrayList;
-
 public class Hotels {
-    private String rooms_available;
-    private int cost;
-    private int length_of_stay;
-    private String choice;
-    
-    
+    private String name;
+    private Integer rooms_available;
+    private Room roomType;
+    /**
+     * Empty constructor
+     */
     public Hotels() {
     }
-    public Hotels(String rooms_available,int cost, int length_of_stay,int Hotel_rating) {
+    /**
+     * Constructor that changes private data fields in the class with the corresponding inputted data fields
+     */
+    public Hotels(String hotel, Integer rooms_available, Room roomType) {
+        this.name = hotel;
+        this.rooms_available = rooms_available;
+        this.roomType = roomType;
     }
-    public void setCost(int cost) {
-        this.cost = cost;
-    }
-        public void setLength_of_stay(int length_of_stay) {
-        this.length_of_stay = length_of_stay;
-    }
-        public void setRooms_available(String rooms_available) {
+    /**
+     * Method that sets what rooms are available to book
+     * 
+     */
+    public void setRooms_available(Integer rooms_available) {
         this.rooms_available = rooms_available;
     }
-
-    
-    public int getCost() {
-        return(cost);
-    }
-        public String getRooms_available() {
-        return(rooms_available);
-    }
-
-        public int getlength_of_stay() {
-        return(length_of_stay);
-    }
-    public ArrayList<Room> getArray(String choice){
-		ArrayList<Room> room;
-		room = getThreeStar();
-		if(choice.equals("3")){
-			room = getThreeStar();
-		}else if(choice.equals("4")){
-			room = getFourStar();
-		}else if(choice.equals("5")){
-			room = getFiveStar();
-		}
-		return room;
-    }
-    public String getChoice() {
-		return choice;
-	}
-
-	public void setChoice(String choice) {
-		this.choice = choice;
-	}
-/*
-Method to check occupancy of a room
- */
-    public int getMaxOccupants(String roomType){
-        int max = 0;
-        for(int i = 0; i < getArray(this.choice).size(); i++){
-            if((roomType.equals(getArray(this.choice).get(i).getRoomType())) == true){
-                max = getArray(this.choice).get(i).getMaxOccupancy();
-                return max;
-            }
-        }
-        return max;
+    /**
+     * Method that gets what rooms are available too book
+     * Returns rooms that are available
+     */
+    public Integer getRooms_available() {
+        return (rooms_available);
     }
 
+    /**
+     * method that gets the type of room
+     * @return the type of room
+     */
+    public Room getRoomType() {
+        return roomType;
     }
+
+    /**
+     * 
+     * method that gets the name of a hotel
+     * @return name of the hotel
+     */
+    public String getName() {
+        return name;
+    }
+     
+     /** method to format the output of data about the hotels
+     * returns the name of the hotel and the type of room, the rate of the room and the purchase type
+     */
+    public String format(Double rate, Boolean isAdvancePurchase) {
+        return name + ", " + roomType.format(rate, isAdvancePurchase);
+    }
+
+    @Override
+    /**
+     * method to format the output of data about the hotels
+     * returns the name of the hotel and the type of room
+     */
+    public String toString() {
+        return name + ", " + roomType;
+    }
+
+//    public ArrayList<Room> getArray(String choice){
+//        ArrayList<Room> room = null;
+////        room = getThreeStar();
+////        if(choice.equals("3")){
+////            room = getThreeStar();
+////        }else if(choice.equals("4")){
+////            room = getFourStar();
+////        }else if(choice.equals("5")){
+////            room = getFiveStar();
+////        }
+//        return room;
+//    }
+//    public String getChoice() {
+//        return choice;
+//    }
+//
+//    public void setChoice(String choice) {
+//        this.choice = choice;
+//    }
+//    /*
+//    Method to check occupancy of a room
+//     */
+//    public int getMaxOccupants(String roomType){
+//        int max = 0;
+//        for(int i = 0; i < getArray(this.choice).size(); i++){
+//            if((roomType.equals(getArray(this.choice).get(i).getRoomType())) == true){
+//                max = getArray(this.choice).get(i).getMaxOccupancy();
+//                return max;
+//            }
+//        }
+//        return max;
+//    }
+}
