d514a9a2c85318b342d4fe9764ea4b18024142b7
rian-ol <64734797+rian-ol@users.noreply.github.com>
2021-11-30 11:09:00 +0000

    Update FileReader.java
    just adding javadocs

--- FileReader.java
+++ FileReader.java
@@ -7,89 +7,105 @@ public class FileReader {
     private String [][] Reservations;
     private String [][] UserPassword;
 
+    /**
+     * reads the file l4Hotels.csv 
+     * and transfers the data into a separate arraylist for future use
+     * @return the arraylist info
+     * @throws FileNotFoundException
+     */
+    public String [][] readHotels() throws FileNotFoundException{
 
-public String [][] readHotels() throws FileNotFoundException{
+        java.io.File file = new java.io.File("l4Hotels.csv");
+        Scanner input = new Scanner(file);
 
-    java.io.File file = new java.io.File("l4Hotels.csv");
-    Scanner input = new Scanner(file);
+        String line;
+        int z = 0;
+        this.info = new String [12][12];
 
-    String line;
-    int z = 0;
-    this.info = new String [12][12];
-
-    while(input.hasNext()){
-        line = input.nextLine();
-        this.info[z] = line.split(",");
-        z++;
+        while(input.hasNext()){
+            line = input.nextLine();
+            this.info[z] = line.split(",");
+            z++;
+        }
+        input.close();
+        return info;
     }
-    input.close();
-    return info;
+
+    /**
+     * reads the file Reservations.csv 
+     * and transfers the data into a separate arraylist for future use
+     * @return the arraylist Reservations
+     * @throws FileNotFoundException
+     */
+    public String [][] readRes() throws FileNotFoundException{
+
+        java.io.File file = new java.io.File("Reservations.csv");
+        Scanner count = new Scanner(file);
+        Scanner input = new Scanner(file);
+
+        String line ;
+        String c;
+        int z = 0;
+
+        // Row Count
+        while(count.hasNext()){
+            c = count.nextLine();
+            z++;
+        }
+        count.close();
+
+        this.Reservations =new String [z][9];
+
+        //Make z=0 again to iterate threw the columns
+        z=0;
+
+        while(input.hasNext()){
+            line = input.nextLine();
+            this.Reservations[z] = line.split(",");
+            z++;
+        }
+        input.close();
+        return Reservations;
+
+
+
+    }
+
+    /**
+     * reads the file UsernamesAndPasswords.csv 
+     * and transfers the data into a separate arraylist for future use
+     * @return the arraylist UserPassword
+     * @throws FileNotFoundException
+     */
+    public String[][] readUserPasswordFile() throws FileNotFoundException{
+        java.io.File File = new java.io.File("UsernamesAndPasswords.csv");
+        Scanner input = new Scanner(File);
+        Scanner count = new Scanner(File);
+
+        String line;
+        String c;
+        int z = 0;
+
+        while(count.hasNext()){
+            c = count.nextLine();
+            z++;
+        }
+        count.close();
+
+        this.UserPassword = new String[z][3];
+        z=0;
+
+        while(input.hasNext()){
+            line = input.nextLine();
+            this.UserPassword[z] = line.split(",");
+            z++;
+        }
+        input.close();
+        return UserPassword;
+
+
+
+    }
+
+
 }
-
-public String [][] readRes() throws FileNotFoundException{
-
-    java.io.File file = new java.io.File("Reservations.csv");
-    Scanner count = new Scanner(file);
-    Scanner input = new Scanner(file);
-
-    String line ;
-    String c;
-    int z = 0;
-
-    // Row Count
-    while(count.hasNext()){
-       c = count.nextLine();
-       z++;
-    }
-    count.close();
-
-    this.Reservations =new String [z][9];
-    
-    //Make z=0 again to iterate threw the columns
-    z=0;
-
-    while(input.hasNext()){
-        line = input.nextLine();
-        this.Reservations[z] = line.split(",");
-        z++;
-    }
-    input.close();
-    return Reservations;
-
-
-
-}
-
-public String[][] readUserPasswordFile() throws FileNotFoundException{
-    java.io.File File = new java.io.File("UsernamesAndPasswords.csv");
-    Scanner input = new Scanner(File);
-    Scanner count = new Scanner(File);
-    
-    String line;
-    String c;
-    int z = 0;
-    
-    while(count.hasNext()){
-        c = count.nextLine();
-        z++;
-    }
-    count.close();
-    
-    this.UserPassword = new String[z][3];
-    z=0;
-
-    while(input.hasNext()){
-        line = input.nextLine();
-        this.UserPassword[z] = line.split(",");
-        z++;
-    }
-    input.close();
-    return UserPassword;
-
-    
-    
-}
-
-
-}
-
